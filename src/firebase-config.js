925c34239ece08ab1fbd5fafbb81f9cede2bbbfd
Victor Gbaye <victorgbayedev@gmail.com>
2022-02-23 16:05:50 +0100

    added ENV variables to secure config details

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAw80isXveA6LJckP1gfMK--sN3ElvvQls",
-  authDomain: "chat-app-a9a6a.firebaseapp.com",
-  projectId: "chat-app-a9a6a",
-  storageBucket: "chat-app-a9a6a.appspot.com",
-  messagingSenderId: "6433924797",
-  appId: "1:6433924797:web:545f552e9cc3d9e65bf9d4",
-  measurementId: "G-HBCPS2110E"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
